c5743800f18d360b36e46d0c3f1fa8bf9129414d
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 22:04:45 +0700

    C:\ssl\BaltimoreCyberTrustRoot.crt.pem

--- dbconnect.php
+++ dbconnect.php
@@ -5,7 +5,8 @@
 // $db_name = 'register';
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306);
+mysqli_ssl_set($conn,NULL,NULL, "C:\ssl\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
