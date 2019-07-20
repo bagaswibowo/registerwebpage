e8aeccd0dab62a626736b2caeb5325448101dbac
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 22:14:00 +0700

    dasd

--- dbconnect.php
+++ dbconnect.php
@@ -7,6 +7,4 @@
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
 mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-
-
 ?>
