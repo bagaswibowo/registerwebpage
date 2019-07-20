119853a2f0a445324ad7a030ed359cba7ed64a54
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 21:19:53 +0700

    pass

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'registerdbmysql.mysql.database.azure.com';
 $username = 'bagaswibowo@registerdbmysql';
-$password = 'advan111';
+$password = 'Advan111';
 $db_name = 'register';
 
 //Establishes the connection
@@ -11,17 +11,17 @@ mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
 
-    if (mysqli_query($conn, '
-    CREATE TABLE IF NOT EXISTS `users` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `username` varchar(100) NOT NULL,
-        `email` varchar(100) NOT NULL,
-        `password` varchar(255) NOT NULL,
-        PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-    ')) {
-    printf("Table created\n");
-    }
+    // if (mysqli_query($conn, '
+    // CREATE TABLE IF NOT EXISTS `users` (
+    //     `id` int(11) NOT NULL AUTO_INCREMENT,
+    //     `username` varchar(100) NOT NULL,
+    //     `email` varchar(100) NOT NULL,
+    //     `password` varchar(255) NOT NULL,
+    //     PRIMARY KEY (`id`)
+    //   ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+    // ')) {
+    // printf("Table created\n");
+    // }
 mysqli_close($conn);
 
 ?>
