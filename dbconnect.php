4e27a1a49c881310b6854619a0330723a213da46
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 21:37:03 +0700

    close

--- dbconnect.php
+++ dbconnect.php
@@ -12,17 +12,4 @@ mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_C
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
 
-    // if (mysqli_query($conn, '
-    // CREATE TABLE IF NOT EXISTS `users` (
-    //     `id` int(11) NOT NULL AUTO_INCREMENT,
-    //     `username` varchar(100) NOT NULL,
-    //     `email` varchar(100) NOT NULL,
-    //     `password` varchar(255) NOT NULL,
-    //     PRIMARY KEY (`id`)
-    //   ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-    // ')) {
-    // printf("Table created\n");
-    // }
-mysqli_close($conn);
-
 ?>
