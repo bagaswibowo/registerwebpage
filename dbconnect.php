47ba49ffe3b07264a6fb896e33018a4136766465
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 22:10:54 +0700

    MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT

--- dbconnect.php
+++ dbconnect.php
@@ -6,7 +6,7 @@
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
-mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
