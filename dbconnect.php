1ecd867296edcf65288d8c922667273cebffd4e7
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 21:44:20 +0700

    mysqli_real_connect

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,14 @@
 <?php
-$host = 'registerdbmysql.mysql.database.azure.com';
-$username = 'bagaswibowo@registerdbmysql';
-$password = 'Advan111';
-$db_name = 'register';
+// $host = 'registerdbmysql.mysql.database.azure.com';
+// $username = 'bagaswibowo@registerdbmysql';
+// $password = 'Advan111';
+// $db_name = 'register';
 
-//Establishes the connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-    if (mysqli_connect_errno($conn)) {
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
-    }
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'register', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 ?>
