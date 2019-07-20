392c072d05d23ccfab91cb6feb6edff3625921c3
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-20 21:34:43 +0700

    --

--- dbconnect.php
+++ dbconnect.php
@@ -7,7 +7,7 @@ $db_name = 'register';
 //Establishes the connection
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306,MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
     if (mysqli_connect_errno($conn)) {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
