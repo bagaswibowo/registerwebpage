1143731edb08456edf63f87d3dbd87ed4a91313b
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-21 21:02:08 +0700

    users

--- load.php
+++ load.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 require_once 'dbconnect.php';
 
-$res = $conn->query("SELECT * FROM ");
+$res = $conn->query("SELECT * FROM users");
 $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
 
 ?>
